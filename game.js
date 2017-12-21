0cebe2d52e009dc35e8e9715cc832c6733e6a05c
Jeff Hou <j.king.hou@gmail.com>
2017-12-21 07:54:13 -0800

    fake commit

--- game.js
+++ game.js
@@ -1,162 +1,207 @@
+var WIDTH = document.body.clientWidth;
+var CENTER_X = WIDTH / 2;
+var MARGIN_LEFT = width / 12;
+var MARGIN_RIGHT = width / 12;
+var IMAGE_WIDTH = width - MARGIN_RIGHT - MARGIN_LEFT;
+var IMAGE_SCALE = IMAGE_WIDTH / 720;
+var HEIGHT = window.innerHeight;
+var IMAGE_Y = 0.34 * HEIGHT;
 
-    class Choice {
-        constructor (image, left_consequence, left_text, right_consequence, right_text) {
-            this.left_consequence = left_consequence;
-            this.left_text = left_text;
-            this.right_consequence = right_consequence;
-            this.right_text = right_text;
-            this.image = game.add.sprite(game.width / 2, 1120, image);
-            this.image.anchor = new Phaser.Point(0.5, 3);
-            this.image.alpha = 0;
-        }
+function nextCard () {
+  bar.angle = 0;
+  bar.choice.reset();
+  inputDisabled = false;
+}
 
-        activate () {
-            this.image.alpha = 1;
-            leftText.text = this.left_text;
-            rightText.text = this.right_text;
-            leftText.x = game.width / 2 - leftText.width / 2 + 140;
-            rightText.x = game.width / 2 - rightText.width / 2 - 140;
-            return this.image;
-        }
+class Consequence {
+  constructor (text, effect) {
+    this.text = text;
+    this.effect = effect;
+  }
+
+  activate () {
+    // TODO: process this.effect
+  }
+}
+
+class CardSprite {
+  constructor (game, imageID) {
+    this.imageID = imageID;
+    this.game = game;
+    this.addSpriteToGame();
+  }
+
+  addSpriteToGame (hidden=true) {
+    this.image = this.game.add.sprite(CENTER_X, 0, this.imageID);
+    this.image.anchor = new Phaser.Point(0.5, 3);
+    this.image.y = this.image.height * 3 + HEIGHT * 0.34;
+
+    var scale = (2 / 3) * (WIDTH / this.image.width);
+    this.image.scale.setTo(scale, scale);
+
+    if (hidden) {
+      this.image.alpha = 0;
+    }
+  }
+
+  hide () {
+    this.image.alpha = 0;
+  }
+
+  show () {
+    this.image.alpha = 1;
+  }
+}
+
+class ReignsGame {
+  constructor () {
+
+  }
+}
+ReignsGame.getInstance = function () {
+  if (ReignsGame.instance != undefined) {
+    return ReignsGame.instance;
+  }
+  ReignsGame.instance = new ReignsGame();
+}
+
+class Choice {
+  constructor (image, left_consequence, right_consequence) {
+    this.left_consequence = left_consequence;
+    this.right_consequence = right_consequence;
+    this.cardSprite = new CardSprite(game, image);
+  }
+
+  activate () {
+    var game = ReignsGame.getInstance();
+    this.cardSprite.show();
+    leftText.text = this.left_text;
+    rightText.text = this.right_text;
+    leftText.x = game.width / 2 - leftText.width / 2 + 140;
+    rightText.x = game.width / 2 - rightText.width / 2 - 140;
+    this.image.choice = this;
+    return this.image;
+  }
+
+  reset () {
+    this.image.y = this.image.height * 3 + HEIGHT * 0.34;
+  }
+}
+
+    if (false) {
+      var width = document.body.clientWidth;
+      MARGIN_LEFT = width / 12;
+      MARGIN_RIGHT = width / 12;
+      IMAGE_WIDTH = width - MARGIN_RIGHT - MARGIN_LEFT;
+      IMAGE_SCALE = IMAGE_WIDTH / 720;
     }
 
-    
-
-    var game = new Phaser.Game(800, 700, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update });
+    var game = new Phaser.Game(document.body.clientWidth, window.innerHeight, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update });
 
     function preload() {
-        game.load.image('cardback', 'cardback.png');
-        game.load.image('squirrel', 'squirrel.png');
+        game.load.image('cardback', 'bgcard.png');
+        game.load.image('reigns_man2', 'reigns_man2.png');
     }
 
     var ball;
 
     function create() {
-        game.stage.backgroundColor = "#f5f5dc";
+      game.stage.backgroundColor = "#bca56b";
 
-        keyQ = game.input.keyboard.addKey(Phaser.Keyboard.Q)
-        card = game.add.graphics();
-        card.lineStyle(10, 0xb318ab, 1);
-        card.drawRoundedRect(-270, 20, 540, 600);
-        /*
-        card.lineStyle(10, 0xcccccc, 1);
-        card.beginFill(0xcccccc);
-        card.drawRoundedRect(-140, 250, 280, 280);
-        */
-        card.x = game.width / 2;
+      keyQ = game.input.keyboard.addKey(Phaser.Keyboard.Q)
+      card = game.add.graphics();
+      card.lineStyle(1, 0x291a0c, 1);
+      card.beginFill(0x291a0c);
+      card.drawRect(0, HEIGHT - 50, width, 50);
+      card.drawRect(0, 0, width, 150);
 
-        game.add.sprite(game.width / 2 - 140, 250, "cardback");
+      cardback = game.add.sprite(game.width / 2, 0, "cardback");
+      var scale = 2 * width / cardback.width / 3;
+      cardback.anchor.x = 0.5;
+      cardback.scale.setTo(scale, scale);
+      cardback.y = HEIGHT * 0.34;
 
-        choices = [];
+      choices = [];
 
-        /*
-        bar = game.add.graphics();
-        bar.lineStyle(10, 0x39aaab, 1);
-        bar.beginFill(0x222222, 0.95);
-        bar.drawRoundedRect(-140, -650, 280, 280);
-        bar.angle = 0;
-        bar.x = game.width / 2;
-        bar.y = 900; */
+      style = { font: "bold 36px Arial", fill: "#000", boundsAlignH: "center", textAlign: "center" };
 
-        style = { font: "bold 36px Arial", fill: "#000", boundsAlignH: "center", textAlign: "center" };
+      total = 0;
+      topText = game.add.text(0, 130, "", style);
+      topText.x = game.width / 2 - topText.width / 2 + 140;
+      topText.setTextBounds(-100, 0, 100, 100)
 
-        
-        total = 0;
-        topText = game.add.text(0, 130, "", style);
-        topText.x = game.width / 2 - topText.width / 2 + 140;
-        topText.setTextBounds(-100, 0, 100, 100)
+      leftText = game.add.text(0, 180, "RIGHT...", style);
+      rightText = game.add.text(0, 180, "LEFT...", style);
 
-        leftText = game.add.text(0, 180, "RIGHT...", style);
-        rightText = game.add.text(0, 180, "LEFT...", style);
-        //text.setTextBounds(0, 0, 800, 150);
+      negative = true;
+      pointer = game.input.pointer1;
+      pointer = game.input.mousePointer;
+      inputDisabled = false;
 
-        negative = true;
-        pointer = game.input.pointer1;
-        pointer = game.input.mousePointer;
-        inputDisabled = false;
-
-        choices.push(new Choice("squirrel", null, "I'M A CAT", null, "I'M A KITTY"));
-        bar = choices[0].activate();
+      choices.push(new Choice("reigns_man2", null, "I'M A CAT", null, "I'M A KITTY"));
+      bar = choices[0].activate();
     }
     function update() {
-    if (Math.abs(bar.angle) > 7.5) {
+      if (Math.abs(bar.angle) > 7.5) {
         console.log("gah");
-    }
-    if (!inputDisabled && pointer.isDown) {
+      }
+      if (!inputDisabled && pointer.isDown) {
         if (pointer.x > game.width / 2 + 100 || pointer.x < game.width / 2 - 100) {
-        if (pointer.x > game.width / 2 + 100) {
+          if (pointer.x > game.width / 2 + 100) {
             console.log("right")
             if (bar.angle < 10) {
             bar.angle += .5;
             }
-        } else if (pointer.x < game.width / 2 - 100) {
+          } else if (pointer.x < game.width / 2 - 100) {
             console.log("left")
             if (bar.angle > -10) {
-            bar.angle -= .5;
+              bar.angle -= .5;
             }
-        }
+          }
         } else {
-        if (bar.angle > 0) {
+          if (bar.angle > 0) {
             bar.angle -= 0.5;
-        } else if (bar.angle < 0) {
+          } else if (bar.angle < 0) {
             bar.angle += 0.5;
-        }
+          }
         }
         console.log(pointer.x + ", " + pointer.y);
-    } else {
+      } else {
         if (Math.abs(bar.angle) > 7.5) {
-        if (bar.y - 750 < game.height) {
+          if (bar.y - bar.height * 3 < game.height) {
             bar.y += parseInt(bar.y / 110);
             bar.angle *= 1.05;
             inputDisabled = true;
-        } else {
+          } else {
             if (bar.angle > 0) {
-            total += 2; //right
-            topText.text = "" + total;
-            bar.angle = 0;
-            bar.y = 1120;
-            inputDisabled = false;
+              total += 2; //right
+              topText.text = "" + total;
+              nextCard();
             } else {
-            //left
-            total -= 2;
-            topText.text = "" + total;
-            bar.angle = 0;
-            bar.y = 1120;
-            inputDisabled = false;
+              //left
+              total -= 2;
+              topText.text = "" + total;
+              nextCard();
             }
-        }
+          }
         } else {
-        if (Math.abs(bar.angle) < 0.3) {
+          if (Math.abs(bar.angle) < 0.3) {
             bar.angle = 0;
-        } else if (bar.angle > 0) {
+          } else if (bar.angle > 0) {
             bar.angle -= 0.3;
-        } else if (bar.angle < 0) {
+          } else if (bar.angle < 0) {
             bar.angle += 0.3;
+          }
         }
-        }
-    }
-    if (bar.angle > 7.5) {
+      }
+      if (bar.angle > 7.5) {
         leftText.alpha = 1; // right
         rightText.alpha = 0;
-    } else if (bar.angle < -7.5) {
+      } else if (bar.angle < -7.5) {
         rightText.alpha = 1; // left
         leftText.alpha = 0;
-    } else {
+      } else {
         leftText.alpha = 0;
         rightText.alpha = 0;
-    } 
-
-    /*
-    if (bar.angle > 10) {
-        negative = !negative;
-    } else if (bar.angle < -10) {
-        negative = !negative;
-    }
-
-    if (negative) {
-        bar.angle -= .1
-    } else {
-        bar.angle += .1
-    }
-    */
+      }
     }
